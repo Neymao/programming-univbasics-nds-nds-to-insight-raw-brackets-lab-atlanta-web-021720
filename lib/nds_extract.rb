68dc0e4a664b79abe87afbe54ed05d81427e4bb2
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-24 21:06:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,6 +32,21 @@ column_index = 0
 end
 
 
+grand_total = 0
+row_index = 0
+while row_index < hash.length do
+  column_index = 0
+  while column_index < hash[row_index].length do
+    inner_len = hash[row_index][column_index].length
+    inner_index = 0
+    while inner_index < inner_len do
+      [:price]
+      inner_index += 1
+    end
+    column_index += 1
+  end
+  row_index += 1
+end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
